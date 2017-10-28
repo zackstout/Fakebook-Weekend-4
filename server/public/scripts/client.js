e6b1e9a52b170999bbb6a69a8978a79f2ea318f9
Zack Stout <zackstout@gmail.com>
2017-10-28 18:17:59 -0500

    code successfully refactored into repeat format

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -17,38 +17,76 @@ picsApp.controller('CommandCenter', function() {
   //
   // cc.likeClick
   //
-  // 
+  //
 
-
-  cc.like1=0;
-  cc.like2=0;
-  cc.like3=0;
-
-  cc.bool1=false;
-  cc.bool2=false;
-  cc.bool3=false;
-
-  cc.liker1 = function() {
-    console.log(cc.like1);
-    cc.like1++;
-  };
-  cc.liker2 = function() {
-    cc.like2++;
-  };
-  cc.liker3 = function() {
-    cc.like3++;
+  var image1 = {
+    path: "styles/images/dogyawn.jpg",
+    description: "HELLO THERE",
+    showPic: true,
+    likes: 0,
+    views: 0
   };
 
-  cc.toggler1 = function() {
-    console.log(cc.bool1);
-    cc.bool1 = !cc.bool1;
+  var image2 = {
+    path: "styles/images/houseonwater.jpg",
+    description: "HI",
+    showPic: true,
+    likes: 0,
+    views: 0
   };
-  cc.toggler2 = function() {
-    cc.bool2 = !cc.bool2;
+
+  var image3 = {
+    path: "styles/images/fieldperson.jpg",
+    description: 'what up!!!',
+    showPic: true,
+    likes: 0,
+    views: 0
   };
-  cc.toggler3 = function() {
-    cc.bool3 = !cc.bool3;
+
+  cc.images = [image1, image2, image3];
+
+  cc.clickImage = function(pic) {
+    console.log('in here', pic.showPic);
+    pic.showPic = !pic.showPic;
+    if (!pic.showPic) {
+      pic.views ++;
+    }
   };
+
+  cc.clickLike = function(pic) {
+    console.log('in likes', pic.likes);
+    pic.likes ++;
+  };
+  //
+  // cc.like1=0;
+  // cc.like2=0;
+  // cc.like3=0;
+  //
+  // cc.bool1=false;
+  // cc.bool2=false;
+  // cc.bool3=false;
+  //
+  // cc.liker1 = function() {
+  //   console.log(cc.like1);
+  //   cc.like1++;
+  // };
+  // cc.liker2 = function() {
+  //   cc.like2++;
+  // };
+  // cc.liker3 = function() {
+  //   cc.like3++;
+  // };
+  //
+  // cc.toggler1 = function() {
+  //   console.log(cc.bool1);
+  //   cc.bool1 = !cc.bool1;
+  // };
+  // cc.toggler2 = function() {
+  //   cc.bool2 = !cc.bool2;
+  // };
+  // cc.toggler3 = function() {
+  //   cc.bool3 = !cc.bool3;
+  // };
 
   cc.commenters1 = [];
 
