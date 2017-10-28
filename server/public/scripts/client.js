52f88f24b242bcb88bcb31b60828f0a29da3b105
Zack Stout <zackstout@gmail.com>
2017-10-28 18:35:19 -0500

    comments section working, just need to add show comments function and clear all input fields

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -6,19 +6,6 @@ var picsApp = angular.module('picsApp', []);
 picsApp.controller('CommandCenter', function() {
   var cc = this;
 
-
-  // var image1 = {
-  //   path, description, boolean
-  // };
-  //
-  // cc.images = [image1, ..];
-  //
-  // cc.imageClick
-  //
-  // cc.likeClick
-  //
-  //
-
   var image1 = {
     path: "styles/images/dogyawn.jpg",
     description: "HELLO THERE",
@@ -66,43 +53,13 @@ picsApp.controller('CommandCenter', function() {
     pic.comments.push(cc.comment);
     console.log(pic.comments);
   };
-  //
-  // cc.like1=0;
-  // cc.like2=0;
-  // cc.like3=0;
-  //
-  // cc.bool1=false;
-  // cc.bool2=false;
-  // cc.bool3=false;
-  //
-  // cc.liker1 = function() {
-  //   console.log(cc.like1);
-  //   cc.like1++;
-  // };
-  // cc.liker2 = function() {
-  //   cc.like2++;
-  // };
-  // cc.liker3 = function() {
-  //   cc.like3++;
-  // };
-  //
-  // cc.toggler1 = function() {
-  //   console.log(cc.bool1);
-  //   cc.bool1 = !cc.bool1;
-  // };
-  // cc.toggler2 = function() {
-  //   cc.bool2 = !cc.bool2;
-  // };
-  // cc.toggler3 = function() {
-  //   cc.bool3 = !cc.bool3;
-  // };
 
-  cc.commenters1 = [];
-
-  cc.commenter1 = function() {
-    console.log(cc.comment1);
-    cc.commenters1.push(cc.comment1);
-    console.log(cc.commenters1);
-  };
+  // cc.commenters1 = [];
+  //
+  // cc.commenter1 = function() {
+  //   console.log(cc.comment1);
+  //   cc.commenters1.push(cc.comment1);
+  //   console.log(cc.commenters1);
+  // };
 
 });
