3357466f737e7fdfd6a856c1d9fafb95b3a9b19f
Zack Stout <zackstout@gmail.com>
2017-10-29 18:18:15 -0500

    put images in their own module, fiddled more with styling

---
+++ server/modules/images.js
@@ -0,0 +1,263 @@
+
+
+var styleWide = {
+  height: '300px',
+  width: 'auto',
+  'margin':'8px',
+  'padding':'8px'
+};
+
+var styleWideBox = {
+  height: '500px',
+  width: 'auto',
+  'border-radius': '12px',
+  'background-color': 'rgba(231, 116, 113, 0.4)',
+  'margin-bottom': '12px',
+  'padding': '8px',
+};
+
+var styleCaptionWide = {
+  width: 'auto',
+  height: '300px',
+  'background-color': 'rgba(231, 116, 113, 0.4)',
+  'margin':'8px',
+  'padding':'8px'
+};
+
+var styleHigh = {
+  height: '420px',
+  width: 'auto',
+  'margin':'8px',
+  'padding':'8px'
+};
+
+var styleHighBox = {
+  width: 'auto',
+  height: '590px',
+  'border-radius': '12px',
+  'background-color': 'rgba(231, 116, 113, 0.4)',
+  'padding': '8px',
+  'margin-bottom': '12px'
+};
+
+var styleCaptionHigh = {
+  width: 'auto',
+  height: '450px',
+  'background-color': 'rgba(231, 116, 113, 0.4)',
+  'margin':'8px',
+  'padding':'8px'
+};
+
+var image0 = {
+  id: 0,
+  path: "styles/images/doogle.jpg",
+  description: "Ready to take on the philosophy scene with tiny doogle",
+  showPic: true,
+  likes: 0,
+  views: 0,
+  comment: '',
+  comments: [],
+  showComments: false,
+  style: styleWide,
+  style2: styleCaptionWide,
+  styleHolder: styleWideBox
+};
+
+var image1 = {
+  id: 1,
+  path: "styles/images/bball.jpg",
+  description: "Shooting hoops in the desert",
+  showPic: true,
+  likes: 0,
+  views: 0,
+  comment: '',
+  comments: [],
+  showComments: false,
+  style: styleWide,
+  style2: styleCaptionWide,
+  styleHolder: styleWideBox
+};
+
+var image2 = {
+  id: 2,
+  path: "styles/images/calf.jpg",
+  description: 'Get that milk bb',
+  showPic: true,
+  likes: 0,
+  views: 0,
+  comment: '',
+  comments: [],
+  showComments: false,
+  style: styleHigh,
+  style2: styleCaptionHigh,
+  styleHolder: styleHighBox
+};
+
+var image3 = {
+  id: 3,
+  path: "styles/images/homies.jpg",
+  description: "Casual at the lake",
+  showPic: true,
+  likes: 0,
+  views: 0,
+  comment: '',
+  comments: [],
+  showComments: false,
+  style: styleWide,
+  style2: styleCaptionWide,
+  styleHolder: styleWideBox
+};
+
+var image4 = {
+  id: 4,
+  path: "styles/images/shorts.jpg",
+  description: "Brotherly love at Renn Fayre",
+  showPic: true,
+  likes: 0,
+  views: 0,
+  comment: '',
+  comments: [],
+  showComments: false,
+  style: styleHigh,
+  style2: styleCaptionHigh,
+  styleHolder: styleHighBox
+};
+
+var image5 = {
+  id: 5,
+  path: "styles/images/reflection.jpg",
+  description: "Sometimes I need to lie down",
+  showPic: true,
+  likes: 0,
+  views: 0,
+  comment: '',
+  comments: [],
+  showComments: false,
+  style: styleWide,
+  style2: styleCaptionWide,
+  styleHolder: styleWideBox
+};
+
+var image6 = {
+  id: 6,
+  path: "styles/images/theweeds.jpg",
+  description: "Chilling at the reservoir",
+  showPic: true,
+  likes: 0,
+  views: 0,
+  comment: '',
+  comments: [],
+  showComments: false,
+  style: styleWide,
+  style2: styleCaptionWide,
+  styleHolder: styleWideBox
+};
+
+var image7 = {
+  id: 7,
+  path: "styles/images/winter.jpg",
+  description: "Winter in the desert",
+  showPic: true,
+  likes: 0,
+  views: 0,
+  comment: '',
+  comments: [],
+  showComments: false,
+  style: styleWide,
+  style2: styleCaptionWide,
+  styleHolder: styleWideBox
+};
+
+var image8 = {
+  id: 8,
+  path: "styles/images/silo.jpg",
+  description: "In the dairy silo",
+  showPic: true,
+  likes: 0,
+  views: 0,
+  comment: '',
+  comments: [],
+  showComments: false,
+  style: styleHigh,
+  style2: styleCaptionHigh,
+  styleHolder: styleHighBox
+};
+
+var image9 = {
+  id: 9,
+  path: "styles/images/sbsb.jpg",
+  description: "Student body meeting",
+  showPic: true,
+  likes: 0,
+  views: 0,
+  comment: '',
+  comments: [],
+  showComments: false,
+  style: styleWide,
+  style2: styleCaptionWide,
+  styleHolder: styleWideBox
+};
+
+var image10 = {
+  id: 10,
+  path: "styles/images/vanity.jpg",
+  description: "The sea taught me vanity",
+  showPic: true,
+  likes: 0,
+  views: 0,
+  comment: '',
+  comments: [],
+  showComments: false,
+  style: styleWide,
+  style2: styleCaptionWide,
+  styleHolder: styleWideBox
+};
+
+var image11 = {
+  id: 11,
+  path: "styles/images/gold.jpg",
+  description: "Back in the day",
+  showPic: true,
+  likes: 0,
+  views: 0,
+  comment: '',
+  comments: [],
+  showComments: false,
+  style: styleWide,
+  style2: styleCaptionWide,
+  styleHolder: styleWideBox
+};
+
+var image12 = {
+  id: 12,
+  path: "styles/images/mononoke.jpg",
+  description: "No remarks necessary",
+  showPic: true,
+  likes: 0,
+  views: 0,
+  comment: '',
+  comments: [],
+  showComments: false,
+  style: styleWide,
+  style2: styleCaptionWide,
+  styleHolder: styleWideBox
+};
+
+var image13 = {
+  id: 13,
+  path: "styles/images/goose.jpg",
+  description: "Beach with the goose",
+  showPic: true,
+  likes: 0,
+  views: 0,
+  comment: '',
+  comments: [],
+  showComments: false,
+  style: styleWide,
+  style2: styleCaptionWide,
+  styleHolder: styleWideBox
+};
+
+var images = [image0, image1, image2, image3, image4, image5, image6, image7, image8, image9, image10, image11, image12, image13];
+
+module.exports = images;
--- server/routes/images_router.js
+++ server/routes/images_router.js
@@ -2,6 +2,7 @@
 var express = require('express');
 var router = express.Router();
 var pg = require('pg');
+var images = require('../modules/images.js');
 
 var config = {
   database: 'deneb',
@@ -11,260 +12,6 @@ var config = {
   idleTimeoutMillis: 30000
 };
 var pool = new pg.Pool(config);
-//
-//
-var styleHigh = {
-  height: '450px',
-  width: 'auto',
-  'margin':'8px',
-  'padding':'8px'
-};
-
-var styleWide = {
-  height: '300px',
-  width: 'auto',
-  'margin':'8px',
-  'padding':'8px'
-};
-
-var styleHighBox = {
-  width: 'auto',
-  height: '600px',
-  'border-radius': '12px',
-  'background-color': 'rgba(231, 116, 113, 0.4)',
-  'margin': '10px',
-  'padding': '10px'
-};
-
-var styleWideBox = {
-  height: '500px',
-  width: 'auto',
-  'border-radius': '12px',
-  'background-color': 'rgba(231, 116, 113, 0.4)',
-  'margin-bottom': '10px',
-  'padding': '10px',
-  'align-content':'center'
-};
-
-var style2 = {
-  width: '350px',
-  height: '350px',
-  'background-color': 'rgba(231, 116, 113, 0.4)'
-
-};
-
-
-var image0 = {
-  id: 0,
-  path: "styles/images/doogle.jpg",
-  description: "Ready to take on the philosophy scene with tiny doogle",
-  showPic: true,
-  likes: 0,
-  views: 0,
-  comment: '',
-  comments: [],
-  showComments: false,
-  style: styleWide,
-  style2: style2,
-  styleHolder: styleWideBox
-};
-
-var image1 = {
-  id: 1,
-  path: "styles/images/bball.jpg",
-  description: "Shooting hoops in the desert",
-  showPic: true,
-  likes: 0,
-  views: 0,
-  comment: '',
-  comments: [],
-  showComments: false,
-  style: styleWide,
-  style2: style2,
-  styleHolder: styleWideBox
-};
-
-var image2 = {
-  id: 2,
-  path: "styles/images/calf.jpg",
-  description: 'Get that milk bb',
-  showPic: true,
-  likes: 0,
-  views: 0,
-  comment: '',
-  comments: [],
-  showComments: false,
-  style: styleHigh,
-  style2: style2,
-  styleHolder: styleHighBox
-};
-
-var image3 = {
-  id: 3,
-  path: "styles/images/homies.jpg",
-  description: "Casual at the lake",
-  showPic: true,
-  likes: 0,
-  views: 0,
-  comment: '',
-  comments: [],
-  showComments: false,
-  style: styleWide,
-  style2: style2,
-  styleHolder: styleWideBox
-};
-
-var image4 = {
-  id: 4,
-  path: "styles/images/shorts.jpg",
-  description: "Brotherly love at Renn Fayre",
-  showPic: true,
-  likes: 0,
-  views: 0,
-  comment: '',
-  comments: [],
-  showComments: false,
-  style: styleHigh,
-  style2: style2,
-  styleHolder: styleHighBox
-};
-
-var image5 = {
-  id: 5,
-  path: "styles/images/reflection.jpg",
-  description: "Sometimes I need to lie down",
-  showPic: true,
-  likes: 0,
-  views: 0,
-  comment: '',
-  comments: [],
-  showComments: false,
-  style: styleWide,
-  style2: style2,
-  styleHolder: styleWideBox
-};
-
-var image6 = {
-  id: 6,
-  path: "styles/images/theweeds.jpg",
-  description: "Chilling at the reservoir",
-  showPic: true,
-  likes: 0,
-  views: 0,
-  comment: '',
-  comments: [],
-  showComments: false,
-  style: styleWide,
-  style2: style2,
-  styleHolder: styleWideBox
-};
-
-var image7 = {
-  id: 7,
-  path: "styles/images/winter.jpg",
-  description: "Winter in the desert",
-  showPic: true,
-  likes: 0,
-  views: 0,
-  comment: '',
-  comments: [],
-  showComments: false,
-  style: styleWide,
-  style2: style2,
-  styleHolder: styleWideBox
-};
-
-var image8 = {
-  id: 8,
-  path: "styles/images/silo.jpg",
-  description: "In the dairy silo",
-  showPic: true,
-  likes: 0,
-  views: 0,
-  comment: '',
-  comments: [],
-  showComments: false,
-  style: styleHigh,
-  style2: style2,
-  styleHolder: styleHighBox
-};
-
-var image9 = {
-  id: 9,
-  path: "styles/images/sbsb.jpg",
-  description: "Student body meeting",
-  showPic: true,
-  likes: 0,
-  views: 0,
-  comment: '',
-  comments: [],
-  showComments: false,
-  style: styleWide,
-  style2: style2,
-  styleHolder: styleWideBox
-};
-
-var image10 = {
-  id: 10,
-  path: "styles/images/vanity.jpg",
-  description: "The sea taught me vanity",
-  showPic: true,
-  likes: 0,
-  views: 0,
-  comment: '',
-  comments: [],
-  showComments: false,
-  style: styleWide,
-  style2: style2,
-  styleHolder: styleWideBox
-};
-
-var image11 = {
-  id: 11,
-  path: "styles/images/gold.jpg",
-  description: "Back in the day",
-  showPic: true,
-  likes: 0,
-  views: 0,
-  comment: '',
-  comments: [],
-  showComments: false,
-  style: styleWide,
-  style2: style2,
-  styleHolder: styleWideBox
-};
-
-var image12 = {
-  id: 12,
-  path: "styles/images/mononoke.jpg",
-  description: "No remarks necessary",
-  showPic: true,
-  likes: 0,
-  views: 0,
-  comment: '',
-  comments: [],
-  showComments: false,
-  style: styleWide,
-  style2: style2,
-  styleHolder: styleWideBox
-};
-
-var image13 = {
-  id: 13,
-  path: "styles/images/goose.jpg",
-  description: "Beach with the goose",
-  showPic: true,
-  likes: 0,
-  views: 0,
-  comment: '',
-  comments: [],
-  showComments: false,
-  style: styleWide,
-  style2: style2,
-  styleHolder: styleWideBox
-};
-
-var images = [image0, image1, image2, image3, image4, image5, image6, image7, image8, image9, image10, image11, image12, image13];
 
 //
 router.put('/likes/:id', function(req, res){
@@ -276,7 +23,7 @@ router.put('/likes/:id', function(req, res){
     }
   }
   res.sendStatus(201);
-});
+}); //end PUT route for likes
 
 router.put('/comments/:id', function(req, res){
   // console.log(req.body.comment);
@@ -287,7 +34,7 @@ router.put('/comments/:id', function(req, res){
     }
   }
   res.sendStatus(201);
-});
+}); //end PUT route for comments
 
 router.put('/views/:id', function(req, res){
   console.log(req.body.id);
@@ -301,7 +48,7 @@ router.put('/views/:id', function(req, res){
     }
   }
   res.sendStatus(201);
-});
+}); //end PUT route for views
 
 router.get('/', function(req, res){
   res.send(images);
